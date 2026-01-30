66f44cabc93f365c6a9463f19bf0d2203476b929
Cristian <cristian@bratash.net>
2026-01-30 18:54:09 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,3 @@
+print("hiya m8 lets play a game! insert a random number. if it divides by 3. FIZZ! and if it divides by 5. BUZZ! if both. FIZZBUZZ!")
+
+number = int(input("enter a number here m8 ---> "))
