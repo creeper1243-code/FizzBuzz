8776d9f844bf70e213ed4f877c434a58c7a69142
Cristian <cristian@bratash.net>
2026-02-06 18:17:38 +0100

    fizzbuzz done

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 print("hiya m8 lets play a game! insert a random number. if it divides by 3. FIZZ! and if it divides by 5. BUZZ! if both. FIZZBUZZ!")
 
+
 number = int(input("enter a number here m8 ---> "))
+
+if number % 3 == 0 and number % 5 == 0:
+    print("FIZZBUZZ!")
+elif number % 3 == 0:
+    print("FIZZ!")
+elif number % 5 == 0:
+    print("BUZZ!")
